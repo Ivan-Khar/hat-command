60718623bcb3c074e1d00d73249305af4e3c21f4
AqUpd <aquupd@ya.ru>
2021-10-12 21:16:42 +0300

    luckperms permissions

--- src/main/java/com/aqupd/aqupdhat/Main.java
+++ src/main/java/com/aqupd/aqupdhat/Main.java
@@ -1,7 +1,10 @@
 package com.aqupd.aqupdhat;
 
+import me.lucko.fabric.api.permissions.v0.Permissions;
 import net.fabricmc.api.ModInitializer;
 import net.fabricmc.fabric.api.command.v1.CommandRegistrationCallback;
+
+import static com.aqupd.aqupdhat.utils.AqLogger.*;
 import static net.minecraft.server.command.CommandManager.*;
 
 import net.minecraft.entity.EquipmentSlot;
@@ -15,28 +18,38 @@ public class Main implements ModInitializer {
     @Override
     public void onInitialize() {
         CommandRegistrationCallback.EVENT.register(((dispatcher, dedicated) -> {
-            dispatcher.register(literal("hat").executes(context -> {
-                ServerPlayerEntity user = context.getSource().getPlayer();
+            dispatcher.register(literal("hat").executes(ctx -> {
+                ServerPlayerEntity user = ctx.getSource().getPlayer();
                 ItemStack hatStack = user.getMainHandStack();
                 ItemStack currentHat = user.getEquippedStack(EquipmentSlot.HEAD).copy();
-                if (hatStack.getItem() == Items.AIR) {
-                    if (!currentHat.isEmpty()){
-                        user.equipStack(EquipmentSlot.HEAD, hatStack);
-                        user.setStackInHand(Hand.MAIN_HAND, currentHat);
+                //logError(String.valueOf(currentHat.getEnchantments()));
+                if (Permissions.check(user, "aqupdhat.hat.usage")) {
+                    if (!currentHat.getEnchantments().toString().contains("minecraft:binding_curse") || Permissions.check(user, "aqupdhat.hat.bypassbinding")) {
+                        if (hatStack.getItem() == Items.AIR) {
+                            if (!currentHat.isEmpty()) {
+                                user.equipStack(EquipmentSlot.HEAD, hatStack);
+                                user.setStackInHand(Hand.MAIN_HAND, currentHat);
+                            } else {
+                                ctx.getSource().sendError(new TranslatableText("You don't have an item in your hand or head."));
+                                return -1;
+                            }
+                        }
+                        if (currentHat.isEmpty()) {
+                            user.equipStack(EquipmentSlot.HEAD, hatStack.copy());
+                            hatStack.setCount(0);
+                        } else {
+                            user.equipStack(EquipmentSlot.HEAD, hatStack);
+                            user.setStackInHand(Hand.MAIN_HAND, currentHat);
+                        }
+                        return 1;
                     } else {
-                        context.getSource().sendError(new TranslatableText("You don't have an item in your hand or head."));
+                        ctx.getSource().sendError(new TranslatableText("You have \"Curse of Binding\" on your head item."));
                         return -1;
                     }
-                }
-                if (currentHat.isEmpty()) {
-                    user.equipStack(EquipmentSlot.HEAD, hatStack.copy());
-                    hatStack.setCount(0);
                 } else {
-                    user.equipStack(EquipmentSlot.HEAD, hatStack);
-                    user.setStackInHand(Hand.MAIN_HAND, currentHat);
+                    ctx.getSource().sendError(new TranslatableText("You don't have permission to do this."));
+                    return -1;
                 }
-
-                return 1;
             }));
         }));
     }
