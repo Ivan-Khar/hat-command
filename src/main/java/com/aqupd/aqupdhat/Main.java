e6767e454a809877f689326bd709388eb7c2a796
AqUpd <aquupd@ya.ru>
2021-10-12 21:55:10 +0300

    Server-side only

--- src/main/java/com/aqupd/aqupdhat/Main.java
+++ src/main/java/com/aqupd/aqupdhat/Main.java
@@ -1,7 +1,7 @@
 package com.aqupd.aqupdhat;
 
 import me.lucko.fabric.api.permissions.v0.Permissions;
-import net.fabricmc.api.ModInitializer;
+import net.fabricmc.api.DedicatedServerModInitializer;
 import net.fabricmc.fabric.api.command.v1.CommandRegistrationCallback;
 
 import static com.aqupd.aqupdhat.utils.AqLogger.*;
@@ -14,9 +14,9 @@ import net.minecraft.server.network.ServerPlayerEntity;
 import net.minecraft.text.TranslatableText;
 import net.minecraft.util.Hand;
 
-public class Main implements ModInitializer {
+public class Main implements DedicatedServerModInitializer {
     @Override
-    public void onInitialize() {
+    public void onInitializeServer() {
         CommandRegistrationCallback.EVENT.register(((dispatcher, dedicated) -> {
             dispatcher.register(literal("hat").executes(ctx -> {
                 ServerPlayerEntity user = ctx.getSource().getPlayer();
