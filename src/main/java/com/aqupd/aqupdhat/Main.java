0c3afbcfa9083719f8fc75464d4ab52be5972e45
AqUpd <aquupd@ya.ru>
2021-10-12 21:25:37 +0300

    check for "Curse Of Binding"

--- src/main/java/com/aqupd/aqupdhat/Main.java
+++ src/main/java/com/aqupd/aqupdhat/Main.java
@@ -14,30 +14,32 @@ import net.minecraft.util.Hand;
 public class Main implements ModInitializer {
     @Override
     public void onInitialize() {
-        CommandRegistrationCallback.EVENT.register(((dispatcher, dedicated) -> {
-            dispatcher.register(literal("hat").executes(context -> {
-                ServerPlayerEntity user = context.getSource().getPlayer();
-                ItemStack hatStack = user.getMainHandStack();
-                ItemStack currentHat = user.getEquippedStack(EquipmentSlot.HEAD).copy();
-                if (hatStack.getItem() == Items.AIR) {
-                    if (!currentHat.isEmpty()){
+        CommandRegistrationCallback.EVENT.register(((dispatcher, dedicated) -> dispatcher.register(literal("hat").executes(ctx -> {
+            ServerPlayerEntity user = ctx.getSource().getPlayer();
+            ItemStack hatStack = user.getMainHandStack();
+            ItemStack currentHat = user.getEquippedStack(EquipmentSlot.HEAD).copy();
+                if (!currentHat.getEnchantments().toString().contains("minecraft:binding_curse") || user.isCreative()) {
+                    if (hatStack.getItem() == Items.AIR) {
+                        if (!currentHat.isEmpty()) {
+                            user.equipStack(EquipmentSlot.HEAD, hatStack);
+                            user.setStackInHand(Hand.MAIN_HAND, currentHat);
+                        } else {
+                            ctx.getSource().sendError(new TranslatableText("You don't have an item in your hand or head."));
+                            return -1;
+                        }
+                    }
+                    if (currentHat.isEmpty()) {
+                        user.equipStack(EquipmentSlot.HEAD, hatStack.copy());
+                        hatStack.setCount(0);
+                    } else {
                         user.equipStack(EquipmentSlot.HEAD, hatStack);
                         user.setStackInHand(Hand.MAIN_HAND, currentHat);
-                    } else {
-                        context.getSource().sendError(new TranslatableText("You don't have an item in your hand or head."));
-                        return -1;
                     }
-                }
-                if (currentHat.isEmpty()) {
-                    user.equipStack(EquipmentSlot.HEAD, hatStack.copy());
-                    hatStack.setCount(0);
+                    return 1;
                 } else {
-                    user.equipStack(EquipmentSlot.HEAD, hatStack);
-                    user.setStackInHand(Hand.MAIN_HAND, currentHat);
+                    ctx.getSource().sendError(new TranslatableText("You have \"Curse of Binding\" on the item in your helmet slot."));
+                    return -1;
                 }
-
-                return 1;
-            }));
-        }));
+        }))));
     }
 }
